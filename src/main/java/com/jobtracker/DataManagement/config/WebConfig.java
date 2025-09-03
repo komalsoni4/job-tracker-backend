960d245d9b3eef70070e43f426641f30adaf0669
Komal-f <KomalKumari@fico.com>
2025-09-03 12:15:27 +0530

    package name corrected

--- src/main/java/com/jobtracker/DataManagement/config/WebConfig.java
+++ src/main/java/com/jobtracker/DataManagement/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.jobtracker.DataManagement;
+package com.jobtracker.DataManagement.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
